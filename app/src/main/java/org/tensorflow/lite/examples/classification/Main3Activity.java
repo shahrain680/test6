1748686eacc277efbb4f910ff2bc6c7dfc1409d7
shahrain680 <shahrin680@gmail.com>
2020-04-29 14:03:01 +0800

    update

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Main3Activity.java
@@ -0,0 +1,24 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Button button = findViewById(R.id.AIButton);
+
+        button.setOnClickListener(v -> {
+            Intent intent=new Intent(this, ClassifierActivity.class);
+            startActivity(intent);
+        });
+    }
+
+}
